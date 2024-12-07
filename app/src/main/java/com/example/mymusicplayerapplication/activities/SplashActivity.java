54e462880c90e376b2987bed48afdf29fd396b11
monimonicaca <guanjie18@163.com>
2024-12-07 21:31:54 +0800

    启动欢迎页

--- app/src/main/java/com/example/mymusicplayerapplication/activities/SplashActivity.java
+++ app/src/main/java/com/example/mymusicplayerapplication/activities/SplashActivity.java
@@ -1,6 +1,5 @@
 package com.example.mymusicplayerapplication.activities;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
@@ -12,13 +11,13 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.example.mymusicplayerapplication.R;
 
-public class MainActivity extends AppCompatActivity {
+public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_saplash);
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
